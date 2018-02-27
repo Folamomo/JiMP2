c1dbe0faa856b4a39eb519f403c6bf72ec23e7f8
Igor Noga <nogaigor@borg.kis.agh.edu.pl>
2018-02-27 11:00:17 +0100

    factorial

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,13 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+    if (value>=13||value<=-13)
+        return 0;
+
+    int result = 1;
+
+    for(;value!=0;value-=(value > 0) ? 1 : -1)
+        result*=value;
+    return result;
 }
+
