ff535aa6d6724f7aa99f0320cce9bcdce3dd6ff8
Folamomo <noga.igor@gmail.com>
2018-03-04 21:04:10 +0100

    Added recursive version

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -3,7 +3,16 @@
 //
 #include "Factorial.h"
 
+int factorialit(int value);
+
+int factorialrec(int value);
+
 int factorial(int value) {
+    //return factorialit(value);
+    return factorialrec(value);
+}
+
+int factorialit(int value) {
     if (value>=13||value<=-13)
         return 0;
 
@@ -14,3 +23,12 @@ int factorial(int value) {
     return result;
 }
 
+int factorialrec(int value) {
+    if (value >= 13 || value <= -13){
+        return 0;
+    }else if (value == 0){
+        return 1;
+    } else {
+        return value * factorialrec(value > 0 ? (value-1) : (value+1));
+    }
+}
