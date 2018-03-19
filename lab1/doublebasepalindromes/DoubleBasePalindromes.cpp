b523cf8b6c2c9b1a0de64da191a53eff41db6a7d
Folamomo <noga.igor@gmail.com>
2018-03-19 22:17:52 +0100

    DoubleBasePalindromes (and lab1) finished

--- lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
+++ lab1/doublebasepalindromes/DoubleBasePalindromes.cpp
@@ -5,14 +5,23 @@
 #include "DoubleBasePalindromes.h"
 #include <string>
 #include <Palindrome.h>
+#include <ReverseString.h>
 
+std::string ToBinStr(int number){
+    std::string result;
+    while (number>0){
+        result += std::to_string(number%2);
+        number>>=1;
+    }
+    return reverse(result);
+}
 
 uint64_t DoubleBasePalindromes(int max_vaule_exculsive){
-    for (int i = 0; i < max_vaule_exculsive; ++i) {
-        std::string str = std::to_string(i);
-        if(is_palindrome(str)){
-            
-        }
-    }
-    return 0;
+    uint64_t sum = 0;
+    for (int i = 0; i < max_vaule_exculsive; ++i)
+        if(is_palindrome( std::to_string(i)))
+            if (is_palindrome(ToBinStr(i)))
+                sum += i;
+    return sum;
 }
+
--- lab1/doublebasepalindromes/main.cpp
+++ lab1/doublebasepalindromes/main.cpp
@@ -2,5 +2,10 @@
 // Created by igor on 06.03.18.
 //
 
+#include <DoubleBasePalindromes.h>
 
+int main(){
+
+    return 0;
+}
 
