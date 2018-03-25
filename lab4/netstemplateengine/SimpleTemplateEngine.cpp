88d38deb5c4093371e5bffcb0d6528db010e15ee
Folamomo <noga.igor@gmail.com>
2018-03-25 16:26:44 +0200

    works without injection

--- lab4/netstemplateengine/SimpleTemplateEngine.cpp
+++ lab4/netstemplateengine/SimpleTemplateEngine.cpp
@@ -4,16 +4,23 @@
 
 #include "SimpleTemplateEngine.h"
 #include <vector>
+#include <regex>
 
 nets::View::View(const std::string &text) {
     text_=text;
 }
 
 std::string nets::View::Render(const std::unordered_map<std::string, std::string> &model) const {
-    std::string output;
-    std::vector<char> c_text(text_.c_str(), text_.c_str() + text_.size() + 1);
-    for (char character : c_text){
-
+    std::string output = text_;
+    std::regex curlies ("\\{\\{\\w+\\}\\}");
+    for (const auto& key : model){
+        std::string replacement = key.second;
+        if(std::regex_match(replacement, curlies)){
+            replacement="";
+        }
+        std::regex to_replace ("\\{\\{"+key.first +"\\}\\}");
+        output=std::regex_replace (output, to_replace, replacement);
     }
+    output=std::regex_replace (output, curlies, "");
     return output;
 }
