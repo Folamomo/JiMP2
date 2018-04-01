d50a3cfdfea6b231d024905649d8e06c0fe4c50c
Folamomo <noga.igor@gmail.com>
2018-04-02 01:08:12 +0200

    Fixed copy constructor bug on non-square matrices

--- lab5/matrix/Matrix.cpp
+++ lab5/matrix/Matrix.cpp
@@ -17,7 +17,7 @@ algebra::Matrix::Matrix() {
 algebra::Matrix::Matrix(size_t dimx, size_t dimy) {
     dimx_=dimx;
     dimy_=dimy;
-    data_=new comp [dimx*dimy];
+    data_=new comp [dimx_*dimy_];
 }
 
 algebra::Matrix::Matrix(const std::string &datastring) {
@@ -28,7 +28,7 @@ algebra::Matrix::Matrix(const algebra::Matrix &other) {
     dimy_=other.dimy_;
     dimx_=other.dimx_;
     data_=new comp [dimx_*dimy_];
-    for (int i = 0; i < dimy_ * dimy_; ++i) {
+    for (int i = 0; i < dimx_ * dimy_; ++i) {
         data_[i]=other.data_[i];
     }
 }
@@ -41,7 +41,7 @@ algebra::Matrix &algebra::Matrix::operator=(const algebra::Matrix &other) {
     dimy_=other.dimy_;
     dimx_=other.dimx_;
     data_=new comp [dimx_*dimy_];
-    for (int i = 0; i < dimy_ * dimy_; ++i) {
+    for (int i = 0; i < dimx_ * dimy_; ++i) {
         data_[i]=other.data_[i];
     }
 }
@@ -114,7 +114,20 @@ algebra::Matrix algebra::Matrix::Sub(const algebra::Matrix &other) const {
 }
 
 algebra::Matrix algebra::Matrix::Mul(const algebra::Matrix &other) const {
-    return algebra::Matrix();
+    if (dimx_!=other.dimy_) {
+        return algebra::Matrix();
+    }
+    Matrix result=Matrix(other.dimx_, dimy_);
+    for (int j = 0; j < dimy_; ++j) {
+        for (int i = 0; i < other.dimx_; ++i) {
+            comp sum = 0.0;
+            for (int k = 0; k < dimx_; ++k) {
+                sum+=this->at(k, j)*other.at(i, k);
+            }
+            result.set(sum, i, j);
+        }
+    }
+    return result;
 }
 
 algebra::Matrix algebra::Matrix::Mul(std::complex<double> &factor) const {
@@ -138,5 +151,27 @@ algebra::Matrix algebra::Matrix::Div(std::complex<double> &factor) const {
 }
 
 algebra::Matrix algebra::Matrix::Pow(int exponent) const {
-    return algebra::Matrix();
+    if (dimx_!=dimy_){
+            return Matrix();
+    }
+    if (exponent==0){
+        Matrix newmatrix=Matrix(dimx_, dimy_);
+        for (int i = 0; i < dimx_; ++i) {
+            for (int j = 0; j < dimy_; ++j){
+                if (i==j)
+                    newmatrix.set(std::complex<double>(1.0), i, j);
+                else
+                    newmatrix.set(std::complex<double>(0.0), i, j);
+            }
+        }
+        return newmatrix;
+    }
+    if (exponent>0){
+        Matrix newmatrix (*this);
+        for (int i = 1; i < exponent; ++i) {
+            newmatrix = newmatrix.Mul(*this);
+        }
+        return newmatrix;
+    }
+    return Matrix();
 }
