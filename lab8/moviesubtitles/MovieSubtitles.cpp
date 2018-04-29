440580ddbd49f1ae862964865eccb1865c2b6ded
Folamomo <noga.igor@gmail.com>
2018-04-29 02:46:28 +0200

    dvd correct cases

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "MovieSubtitles.h"
+
+void moviesubs::MicroDvdSubtitles::ShiftAllSubtitlesBy(int delay, int framerate,
+                                                        std::stringstream* in, std::stringstream* out) {
+    std::regex sub_line_regex(R"(\{(-?\d+)\}\{(-?\d+)\}(.+))");
+    if (framerate < 0){
+        throw std::invalid_argument("Negative framerate");
+    }
+    int shift_by=(delay*framerate)/1000;
+    int line_index=1;
+    char line_buffer[1000];
+    while(in->getline(line_buffer, 1000)) {
+        std::cmatch sub_line;
+        if (std::regex_match(line_buffer, sub_line, sub_line_regex)){
+            int start_frame = std::stoi(sub_line[1])+shift_by;
+            int end_frame = std::stoi(sub_line[2])+shift_by;
+            if (start_frame<end_frame){
+                //TODO: throw(SubtitleEndBeforeStart);
+            }
+            if (start_frame < 0){
+                //TODO: throw(NegativeFrameAfterShift);
+            }
+            (*out)<<"{"<<start_frame<<"}{"<<end_frame<<"}"<<sub_line[3]<<"\n";
+        } else{
+            //TODO: throw(InvalidSubtitleLineFormat);
+        }
+        ++line_index;
+    }
+}
--- lab8/moviesubtitles/MovieSubtitles.h
+++ lab8/moviesubtitles/MovieSubtitles.h
@@ -5,9 +5,16 @@
 #ifndef JIMP_EXERCISES_MOVIESUBTITLES_H
 #define JIMP_EXERCISES_MOVIESUBTITLES_H
 
+#include <regex>
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
 namespace moviesubs {
     class MicroDvdSubtitles{
-
+    public:
+        void ShiftAllSubtitlesBy(int delay, int framerate,
+                                 std::stringstream* in, std::stringstream* out);
     };
 }
 
